edfcbffe7dc66e66c2a907db7e700cc34082ab8f
kingdvd <davidvgoodwin@gmail.com>
2024-09-06 13:00:37 -0400

    added sun and pluto :p

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let planets = [
+    {name: "Sun", inner: true, diameter: 865370, color: "#FFFF00"},
     {name: "Mercury", inner: true, diameter: 3031.9, color:"#696969"},
     {name: "Venus" , inner: true, diameter: 7520.8, color: "#b89165"},
     {name: "Earth" , inner: true, diameter: 7917.5, color: "#5a5b86"},
@@ -6,7 +7,8 @@ let planets = [
     {name: "Jupiter" , inner: false, diameter: 86881, color: "#f6a049"},
     {name: "Saturn" , inner: false, diameter: 72367, color: "#dcd3a1"},
     {name: "Uranus" , inner: false, diameter: 31518, color: "#b4d9df"},
-    {name: "Neptune" , inner: false, diameter: 30599, color: "#456eff"}
+    {name: "Neptune" , inner: false, diameter: 30599, color: "#456eff"},
+    {name: "Pluto", inner: false, diameter: 1476, color: "#ccba99"}
 ];
 
 let indexNode = document.querySelector("#index");
