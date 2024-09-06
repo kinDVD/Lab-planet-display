31955b026601f171f2e61c668c4e2fb7dbd3e324
kingdvd <davidvgoodwin@gmail.com>
2024-09-06 09:18:51 -0400

    updating code to patch display bug

--- script.js
+++ script.js
@@ -1,98 +1,58 @@
-let planet = [];
-planet[0] = {name: "Mercury", inner: true, diameter: 3031.9, color: "#696969"};
-planet[1] = {name: "Venus", inner: true, diameter: 7520.8, color: "#b89165"}
-planet[2] = {name: "Earth",inner: true, diameter: 7917.5, color: "#5a5b86"}
-planet[3] = {name: "Mars", inner: true, diameter: 4212.3, color: "#df8c4e"}
-planet[4] = {name: "Jupiter", inner: false, diameter: 86881, color:"#f6a049"}
-planet[5] = {name: "Saturn", inner: false, diameter: 72367, color: "#dcd3a1"}
-planet[6] = {name: "Uranus", inner: false, diameter: 31518, color:"#b4d9df"}
-planet[7] = {name: "Neptune", inner: false, diameter: 30599, color: "#456eff"}
-
-function listplanets(){
-    planet.forEach(p => console.log(p));
-}
-
-function avgDiameter(){
-let dTotal = 0;    
-planet.forEach(p => console.log(p.name, p.diameter))
-    planet.forEach(p=>{
-        dTotal = p.diameter + dTotal;
-    })
-    let dAverage = dTotal / planet.length;
-    console.log("Average Diameter: " + dAverage);
-}
+let planets = [
+    {name: "Mercury", inner: true, diameter: 3031.9, color:"#696969"},
+    {name: "Venus" , inner: true, diameter: 7520.8, color: "#b89165"},
+    {name: "Earth" , inner: true, diameter: 7917.5, color: "#5a5b86"},
+    {name: "Mars" , inner: true, diameter: 4212.3, color: "#df8c4e"},
+    {name: "Jupiter" , inner: false, diameter: 86881, color: "#f6a049"},
+    {name: "Saturn" , inner: false, diameter: 72367, color: "#dcd3a1"},
+    {name: "Uranus" , inner: false, diameter: 31518, color: "#b4d9df"},
+    {name: "Neptune" , inner: false, diameter: 30599, color: "#456eff"}
+];
 
 let indexNode = document.querySelector("#index");
-let pNameNode = document.querySelector("#pname")
-let pInOutNode = document.querySelector("#pinout")
 let index = 0;
-let planetName = planet[index].name;
-let pInOut = "";
-let pHeight = (planet[index].diameter * 0.01) + "px";
-let dHeigh = document.getElementById("25px").style.transform = "height: " + pHeight;
-let pWidth = (planet[index].diameter * 0.01) + "px";
-let pColor = planet[index].color;
-const screenElement = document.getElementById('div');
-
-
-let nextNode = document.querySelector("#next");
-function nextPlanet(){
-    if(index == planet.length){
-        buttonNode.disabled = true;
-    }
-    else{
-        index++;
-    }
-    planetName = planet[index].name;
-    pNameNode.innerText = planetName;
-    indexNode.innerText = "Index: " + index;
-    
-    let pInO = planet[index].inner;
-    if(pInO === false){
-        pInOut = "Outer Planet";
-        pInOutNode.innerText = pInOut;
-    }
-    else{
-        pInOut = "Inner Planet";
-        pInOutNode.innerText = pInOut;
-    }
-
-    // pHeight = (planet[index].diameter * 0.01) + "px";
-    // pWidth = (planet[index].diameter * 0.01) + "px";
-    // pColor = planet[index].color;
-    // pDot.setAttribute("dot", pHeight);
-    // pDot.setAttribute("width", pWidth);
-    // pDot.setAttribute("background-color", pColor);
-
-}
-
+let nameNode = document.querySelector("#name");
+let innerNode = document.querySelector("#inner");
+let visualNode = document.querySelector("#visual");
 let previousNode = document.querySelector("#previous");
-function previousPlanet(){
-    if(index == 0){
-        buttonNode.disabled = true;
-    }
-    else{
-        index--;
-    }
-    planetName = planet[index].name;
-    pNameNode.innerText = planetName;
-    indexNode.innerText = "Index: " + index;
+let nextNode = document.querySelector("#next");
+updatePlanetDisplay();
 
-    let pInO = planet[index].inner;
-    if(pInO === false){
-        pInOut = "Outer Planet";
-        pInOutNode.innerText = pInOut;
+
+function changeIndex(val){
+    index += val;
+    if(index < 0){
+        index = 0;
     }
-    else{
-        pInOut = "Inner Planet";
-        pInOutNode.innerText = pInOut;
+    else if (index >= planets.length){
+        index = planets.length - 1;
     }
+    indexNode.innerText = `index: ${index}`;
+    updatePlanetDisplay();
 }
 
-
-
-
-
-
-
-
+function updatePlanetDisplay(){
+    nameNode.innerText = planets[index].name;
+    if(planets[index].inner === true){
+        innerNode.innerText = "Inner Planet";
+    }
+    else{
+        innerNode.innerText = "Outer Planet";
+    }
+    visualNode.style.backgroundColor = planets[index].color;
+    visualNode.style.height = (planets[index].diameter / 100) + "px";
+    visualNode.style.width = (planets[index].diameter / 100) + "px";
+    //disable button
+    if(index == 0){
+        previousNode.disabled = true;
+        nextNode.disabled = false;
+    }
+    else if(index == planets.length - 1){
+        previousNode.disabled = false;
+        nextNode.disabled = true;
+    }
+    else{
+        previousNode.disabled = false;
+        nextNode.disabled = false;
+    }
+}
